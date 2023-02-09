04b9e63e0a5446ffea8dea56e479c8be80948f9a
dellius-alexander <dellius.alexander@gmail.com>
2023-02-09 03:45:00 -0500

    updated project docs and initialization

--- SmartScraper/init.sh
+++ SmartScraper/init.sh
@@ -15,45 +15,25 @@ __help(){
     /bin/sh ${0} \\
     [COMMAND]{run|emulate|plugin|build|serve|cordova_serve|help|HELP|-h|--help|add|remove|rm} \\
     [PLATFORMS]{browser|ios|android|osx}
-    """;
+    """
 }
-
 __run(){
-  COMMAND="${1:-$( [[ ${1} =~ ^(run)$ ]] && echo ${1}  || echo "bad run command."; exit 1; )}"
-  OPTIONS="${2}"
-  case ${OPTIONS} in
+COMMAND="${1:-$( [[ ${1} =~ ^(run)$ ]] && echo ${1}  || echo "bad run command."; exit 1; )}";
+OPTIONS="${2}";
+
+if [ -f ./.env.${NODE_ENV}.local ]; then
+  export $( cat ./.env.${NODE_ENV}.local | grep -v '#' | awk '/=/ {print $1}' &2>/dev/null);
+fi
+
+case ${OPTIONS} in
   gh-pages)
     npm install gh-pages --save-dev;
-    export $( cat ./.env.${NODE_ENV}.local | grep -v '#' | awk '/=/ {print $1}');
-
     npm audit fix;
-
-    npx gh-pages -d 'www' -m 'github pages update' -b 'gh-pages'  \
-    -u 'Dellius Alexander <dellius.alexander@gmail.com>'
-
-#    -V, --version            output the version number
-#    -d, --dist <dist>        Base directory for all source files
-#    -s, --src <src>          Pattern used to select which files to publish (default: "**/*")
-#    -b, --branch <branch>    Name of the branch you are pushing to (default: "gh-pages")
-#    -e, --dest <dest>        Target directory within the destination branch (relative to the root) (default: ".")
-#    -a, --add                Only add, and never remove existing files
-#    -x, --silent             Do not output the repository url
-#    -m, --message <message>  commit message (default: "Updates")
-#    -g, --tag <tag>          add tag to commit
-#    --git <git>              Path to git executable (default: "git")
-#    -t, --dotfiles           Include dotfiles
-#    -r, --repo <repo>        URL of the repository you are pushing to
-#    -p, --depth <depth>      depth for clone (default: 1)
-#    -o, --remote <name>      The name of the remote (default: "origin")
-#    -u , --user <address>     The name and email of the user (defaults to the git config).  Format is "Your Name <email@example.com>".
-#    -v, --remove <pattern>   Remove files that match the given pattern (ignored if used together with --add). (default: ".")
-#    -n, --no-push            Commit only (with no push)
-#    -f, --no-history         Push force new commit without parent history
-#    --before-add <file>      Execute the function exported by <file> before "git add"
-#    -h, --help               output usage information
+    npx gh-pages -d 'www' -m 'github pages update' -b 'gh-pages' -u 'Dellius Alexander <dellius.alexander@gmail.com>';
+    echo
     ;;
   browser)
-    echo "Running ${PLATFORMS}........"
+    echo "Running ${PLATFORMS}........";
     npm install;
     node scripts/build.js;
     cordova build -- --live-reload  --debug;
@@ -68,7 +48,7 @@ __run(){
     *)
       __help
       ;;
-  esac
+esac
 }
 #####################################################################
 COMMAND=${1:-$( [[ ${1} =~ ^(run|emulate|plugin|build|serve|cordova_serve|help|HELP|-h|--help)$ ]] && echo ${1} &2>/dev/null )} # Defaults to run -- platform
